ceb7c860b6fc1b97c5df6950f7a50fb9ed148ea8
Luiz Otávio Lima <luiz.oflima@outlook.com>
2022-04-12 20:20:54 -0300

    Meu primeiro commit

---
+++ 03-ConhecendoR_RStudio_Variaveis.R
@@ -0,0 +1,45 @@
+# Objetivo: primeiro contato com a linguagem R e RStudio
+
+# Como?
+#Navegação e Exemplos
+
+# Criando variáveis
+# Para executar clique sobre a linha ou selecione o conjunto de linhas
+# e precione Ctrl + enter
+
+a <- 10
+a
+
+b
+b = 10
+b
+B
+
+# Utilizaremos <- na criação de variáveis e = nas atribuições de funções
+
+c -> 10
+10 -> c
+c -> d
+
+# Para padronizar sempre utilizaremos "variavel" <- "valor"
+
+a <- 10
+b <- 5
+
+c <- a + b
+
+a <- "Heitor"
+b <- "Joao"
+
+c <- a + b
+
+# Funções
+# Fazem parte de um pacote.
+# Pacotes contem muitas funções e precisam estar instalados.
+# Não sendo um pacote padrão ele precisa ser chamado.
+
+c <- c(a, b)
+c
+
+# Help
+?c
